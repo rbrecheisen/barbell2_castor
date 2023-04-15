b9b1db524d367dedd70bb15cc6dfc59fb1935dcb
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2023-04-16 00:15:34 +0200

    easy package import

--- barbell2_castor/__init__.py
+++ barbell2_castor/__init__.py
@@ -1,3 +1,7 @@
 __author__ = 'Ralph Brecheisen'
 __email__ = 'r.brecheisen@maastrichtuniversity.nl'
 __version__ = '0.4.0'
+
+from barbell2_castor.api import CastorApiClient
+from barbell2_castor.castor2sqlite import CastorToSqlite3
+from barbell2_castor.query import CastorQuery
